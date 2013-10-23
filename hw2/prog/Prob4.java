6fa7a3b89c6015237db800aaa69c6b1bdaffcaa9
Tyrone <tph2107@columbia.edu>
2013-10-23 18:48:57 -0400

    Problem 4 done

--- hw2/prog/Prob4.java
+++ hw2/prog/Prob4.java
@@ -4,20 +4,7 @@ import java.io.*;
 import java.util.HashMap;
 
 public class Prob4{
-	//records the counts for the terminal words in the tree in a given HashMap. Mutates the external HashMap
-	public static void getWordCounts(JSONArray tree, HashMap<String, Integer> counts){
-		if(tree.size() == 2){ //rule is unary
-			String word = (String)tree.get(1);
-			if(counts.containsKey(word))
-				counts.put(word, counts.get(word)+1);
-			else
-				counts.put(word, 1);
-		}else{ //rule is binary
-			getWordCounts((JSONArray)tree.get(1), counts);
-			getWordCounts((JSONArray)tree.get(2), counts);
-		}
-	}
-
+	
 	public static JSONArray replaceRares(JSONArray tree, HashMap<String, Integer> rares){
 		if(tree.size() == 2){
 			if(rares.containsKey(tree.get(1)))
@@ -30,32 +17,52 @@ public class Prob4{
 	}
 
 	public static void main(String[] args){
-		if(args.length < 1){
-			System.out.println("usage: java Prob4 <training_file>");
+		if(args.length < 3){
+			System.out.println("usage: java Prob4 <counts_file> <training_file> <replacement_file>");
 			return;
 		}
 		try{
-			FileReader trainFR = new FileReader(args[0]);
+			FileReader countFR = new FileReader(args[0]);
+			FileReader trainFR = new FileReader(args[1]);
+			FileWriter repFW = new FileWriter(args[2]);
+			BufferedReader countBR = new BufferedReader(countFR);
 			BufferedReader trainBR = new BufferedReader(trainFR);
+			BufferedWriter repBW = new BufferedWriter(repFW, 1);
 			String line = null;
-			JSONParser parser = new JSONParser();
 
 			//get counts for each word among all the trees
 			HashMap<String, Integer> counts = new HashMap<String, Integer>();
-			while((line = trainBR.readLine()) != null){
-				JSONArray tree = (JSONArray)parser.parse(line);
-				getWordCounts(tree, counts);
+			while((line = countBR.readLine()) != null){
+				String[] parts = line.split(" ");
+				if(parts[1].equals("UNARYRULE")){
+					String word = parts[3];
+					if(counts.containsKey(word))
+						counts.put(word, counts.get(word) + Integer.parseInt(parts[0]));
+					else
+						counts.put(word, Integer.parseInt(parts[0]));
+				}
 			}
+			countFR.close();
 
 			//find rare words within counts
 			HashMap<String, Integer> rares = new HashMap<String, Integer>();
 			for(String word : counts.keySet()){
 				if(counts.get(word) < 5)
-					rares.put(word, 0);
+					rares.put(word, counts.get(word));
 			}
-			System.out.println(rares);
+
+			JSONParser parser = new JSONParser();
+			while((line = trainBR.readLine()) != null){
+				JSONArray tree = (JSONArray)parser.parse(line);
+				repBW.write(replaceRares(tree, rares).toString()+"\n");
+			}
+			trainFR.close();
+			repFW.close();
+			System.out.println(line);
+
 
 			trainFR.close();
+
 		}catch(Exception e){
 			System.out.println(e);
 		}
