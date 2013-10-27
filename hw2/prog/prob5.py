3095b7cca0690b96046d17b088f0f32c1d31445f
cog-arch <tph2107@columbia.edu>
2013-10-27 15:26:39 -0400

    faster rare-check, currently in first test

--- hw2/prog/prob5.py
+++ hw2/prog/prob5.py
@@ -5,25 +5,30 @@ from math import log
 def getRuleCounts(fname):
 	cf = open(fname)
 	cfStr = cf.read()
+	cf.close()
 	counts = dict()
 	rules = dict()
+	comm = dict()
 	for line in cfStr.split("\n"):
 		lArr = line.split(" ")
 		if len(lArr) == 3:
-			counts[lArr[2]] = (lArr[0], dict())
+			counts[lArr[2]] = (int(lArr[0]), dict())
 		elif len(lArr) == 4:
-			counts[lArr[2]][1][lArr[3]] = (lArr[0], dict())
+			if lArr[3] in comm:
+				comm[lArr[3]] += int(lArr[0])
+			else:
+				comm[lArr[3]] = int(lArr[0])
+			counts[lArr[2]][1][lArr[3]] = (int(lArr[0]), dict())
 		elif len(lArr) == 5:
 			if lArr[2] not in rules:
 				rules[lArr[2]] = []
 			rules[lArr[2]].append((lArr[3], lArr[4]))
 			if lArr[3] in counts[lArr[2]][1]:
-				counts[lArr[2]][1][lArr[3]][1][lArr[4]] = lArr[0]
+				counts[lArr[2]][1][lArr[3]][1][lArr[4]] = int(lArr[0])
 			else:
 				counts[lArr[2]][1][lArr[3]] = (0, dict())
-				counts[lArr[2]][1][lArr[3]][1][lArr[4]] = lArr[0]
-	cf.close()
-	return (rules, counts)
+				counts[lArr[2]][1][lArr[3]][1][lArr[4]] = int(lArr[0])
+	return (rules, counts, comm)
 
 #gets a nonterminal or binary/unary rule count using the dynamic table
 def getCount(table, *args):
@@ -43,19 +48,23 @@ def getCount(table, *args):
 #function for maximum likelihood estimate
 def getQ(table, *args):
 	if len(args) == 2 and args[0] in table:
-		return float(getCount(table, args[0], args[1])) / float(getCount(table, args[0]))
+		return float(getCount(table, args[0], args[1])) / getCount(table, args[0])
 	elif len(args) == 3 and args[0] in table:
-		return float(getCount(table, args[0], args[1], args[2])) / float(getCount(table, args[0]))
+		return float(getCount(table, args[0], args[1], args[2])) / getCount(table, args[0])
 	else:
 		return 0
 
 def stored(table, i, j, x):
 	return i in table and j in table[i] and x in table[i][j]
 
-def pi(table, pt, rules, words, i, j, x):
+def pi(table, pt, rules, wc, words, i, j, x):
 	#print("%s %d-%d" % (x,i,j))
 	if(i == j):
-		return ([x, words[i-1]], getQ(table, x, words[i-1]))
+		#correct for rare words
+		if words[i-1] not in wc or wc[words[i-1]] < 5:
+			return ([x, words[i-1]], getQ(table, x, "_RARE_"))
+		else:
+			return ([x, words[i-1]], getQ(table, x, words[i-1]))
 	aMax = [x, '~', '~']
 	pMax = 0
 	if x in rules:
@@ -65,11 +74,11 @@ def pi(table, pt, rules, words, i, j, x):
 				if stored(pt, i, s, y):
 					py = pt[i][s][y]
 				else:
-					py = pi(table, pt, rules, words, i, s, y)
+					py = pi(table, pt, rules, wc, words, i, s, y)
 				if stored(pt, s+1, j, z):
 					pz = pt[s+1][j][z]
 				else:
-					pz = pi(table, pt, rules, words, s+1, j, z)
+					pz = pi(table, pt, rules, wc, words, s+1, j, z)
 				q = getQ(table, x, y, z)
 				if py[1] > 0 and pz[1] > 0 and q > 0:
 					p = abs(log(q, 2) + log(py[1], 2) + log(pz[1], 2))
@@ -88,9 +97,12 @@ if __name__ == "__main__":
 		print("usage: python %s <counts_file> <dev_file>" % argv[0])
 	else:
 		nfo = getRuleCounts(argv[1])
-		rules = nfo[0]
-		cTable = nfo[1] #cTable.keys() gives all nonterms
+		cTable = nfo[1] #all the rule counts
+		bins = nfo[0] #binary rules
+		wc = nfo[2] # word counts
 		devF = open(argv[2])
+
+		#find arg max(Pt) for each line
 		for line in devF.read().split("\n"):
 			words = line.split(" ")
 			#set up the table
@@ -99,15 +111,16 @@ if __name__ == "__main__":
 				piTable[i] = dict()
 				for j in range(i+1,len(words)+1):
 					piTable[i][j] = dict()
-			pv = pi(cTable, piTable, rules, words, 1, len(words), "S")
+			pv = pi(cTable, piTable, bins, wc, words, 1, len(words), "S")
 			if(pv[1] > 0):
 				print(pv[0])
 			else: #if we're dealing w/ a sentence fragment
 				aMax = []
 				pMax = 0
 				for x in cTable.keys():
-					pv = pi(cTable, piTable, rules, words, 1, len(words), x)
+					pv = pi(cTable, piTable, bins, wc, words, 1, len(words), x)
 					if(pv[1] > pMax):
 						pMax = pv[1]
 						aMax = pv[0]
 				print(aMax)
+	devF.close()
