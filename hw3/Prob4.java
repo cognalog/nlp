bcc70ba43567bd4c1043e945833f4b8fa3c06007
cog-arch <tph2107@columbia.edu>
2013-11-12 02:13:35 -0500

    changed file io and excpetion throwing

--- hw3/Prob4.java
+++ hw3/Prob4.java
@@ -4,58 +4,63 @@ import java.io.*;
 
 public class Prob4{
 	static BufferedReader engFile;
-	static BufferedReader gerFile;
+	static BufferedReader forFile;
+	static String eFName;
+	static String fFName;
 	static PrintStream stdout = System.out;
 
-	public static double initialT(String word){
+	private static void resetReaders() throws FileNotFoundException{
+		engFile = new BufferedReader(new FileReader(eFName));
+		forFile = new BufferedReader(new FileReader(fFName));
+	}
+
+	/*
+	* gives an initial value for t(f | e) for a particular e regardless of f
+	*/
+	private static double initialT(String word) throws IOException{
 		ArrayList<Integer> lines = new ArrayList();
 		HashMap<String, Integer> emissions = new HashMap();
 		String line;
+		
 		int i= 0;
+		//find which english corpus lines contain the word
+		while((line = engFile.readLine()) != null){
+			if(line.indexOf(word) > -1)
+				lines.add(i);
+			i++;
+		}
+		/*
+		* now, get dupeless list of all german words on those lines
+		* the i variable now keeps track of your place in the lines list
+		*/
+		i = 0;
 		int j = 0;
-
-		try{
-			//find which english corpus lines contain the word
-			while((line = engFile.readLine()) != null){
-				if(line.indexOf(word) > -1)
-					lines.add(i);
+		while((line = forFile.readLine()) != null){
+			if(i < lines.size() && j == lines.get(i) || word.equals("_NULL_")){
+				String[] words = line.split(" ");
+				for(String f : words){
+					emissions.put(f, 0);
+				}
 				i++;
 			}
-			/*
-			* now, get dupeless list of all german words on those lines
-			* the i variable now keeps track of your place in the lines list
-			*/
-			i = 0;
-			while((line = gerFile.readLine()) != null){
-				if(i < lines.size() && j == lines.get(i)){
-					String[] words = line.split(" ");
-					for(String f : words){
-						emissions.put(f, 0);
-					}
-					i++;
-				}
-				j++;
-			}
+			j++;
 		}
-		catch(IOException e){
-			stdout.println(e);
-			return 0;
-		}
-		return 1.0 / emissions.size();
+		return (emissions.size() > 0) ? 1.0 / emissions.size() : 0;
 	}
+/*
+	public static HashMap<String, HashMap<String, Double>> emAlg(int iterations){
 
-	public static void main(String[] args){
+	}
+*/
+	public static void main(String[] args) throws IOException{
 		if(args.length < 2){
 			stdout.println("usage: java Prob4 <english_corpus> <german_corpus>");
 			return;
 		}
-		try{
-			//make readers for english and german corpora
-			engFile = new BufferedReader(new FileReader(args[0]));
-			gerFile = new BufferedReader(new FileReader(args[1]));	
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
+		//make readers for english and german corpora
+		eFName = args[0];
+		fFName = args[1];
+		resetReaders();
+		stdout.println(initialT("resumption"));
 	}
 }
